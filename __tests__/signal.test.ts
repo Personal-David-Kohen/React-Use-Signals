b6fb6ca4355f6a8faef1d53b4f9a377fe9736e9b
David-Itzhak-Kohen <114672677+David-Itzhak-Kohen@users.noreply.github.com>
2023-12-07 21:49:36 +0200

    Fixed batch tests

--- __tests__/signal.test.ts
+++ __tests__/signal.test.ts
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
-import { act, renderHook } from '@testing-library/react';
+import { renderHook } from '@testing-library/react';
 import { createSignal, signalEffect } from '../src/utilities/signal.utility';
 
-test('Counter Signals Test', () => {
+test('Counter Signals Test', async () => {
   const counter = createSignal(0);
 
   const callback = jest.fn();
@@ -12,18 +12,24 @@ test('Counter Signals Test', () => {
   counter.value = 2;
   counter.value = 3;
 
-  expect(callback).toHaveBeenCalledTimes(3);
+  // Wait for the next microtask to run
+  //This tests batch updates
+  await new Promise<void>(resolve => queueMicrotask(() => resolve()));
+
+  expect(callback).toHaveBeenCalledTimes(1);
   expect(counter.value).toBe(3);
 
   counter.value = 4;
   counter.value = 5;
   counter.value = 6;
 
-  expect(callback).toHaveBeenCalledTimes(6);
+  await new Promise<void>(resolve => queueMicrotask(resolve));
+
+  expect(callback).toHaveBeenCalledTimes(2);
   expect(counter.value).toBe(6);
 });
 
-test('React Signal Test', () => {
+test('React Signal Test', async () => {
   const counterSignal = createSignal(0);
 
   const effectCallback = jest.fn();
@@ -41,11 +47,13 @@ test('React Signal Test', () => {
     }, [counter]);
   });
 
+  await new Promise<void>(resolve => queueMicrotask(resolve));
+
   expect(effectCallback).toHaveBeenCalledTimes(1);
   expect(useEffectCallback).toHaveBeenCalledTimes(1);
 });
 
-test('Complex Signal Test', () => {
+test('Complex Signal Test', async () => {
   const MOCK_USER = {
     name: 'John',
     age: 20,
@@ -64,18 +72,32 @@ test('Complex Signal Test', () => {
     effectCallback(userSignal.value);
   });
 
+  await new Promise<void>(resolve => queueMicrotask(resolve));
+
   expect(effectCallback).toHaveBeenCalledTimes(1);
 
   userSignal.value.name = 'Jane';
+
+  await new Promise<void>(resolve => queueMicrotask(resolve));
+
   expect(effectCallback).toHaveBeenCalledTimes(2);
 
   userSignal.value.address.city = 'Los Angeles';
+
+  await new Promise<void>(resolve => queueMicrotask(resolve));
+
   expect(effectCallback).toHaveBeenCalledTimes(3);
 
   userSignal.value.address.state = 'CA';
+
+  await new Promise<void>(resolve => queueMicrotask(resolve));
+
   expect(effectCallback).toHaveBeenCalledTimes(4);
 
   userSignal.value.age = 21;
+
+  await new Promise<void>(resolve => queueMicrotask(resolve));
+
   expect(effectCallback).toHaveBeenCalledTimes(5);
 
   expect(userSignal.value).toEqual({
